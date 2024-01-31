af7b6c23924e4ac97dcfd722e53e613fd2d9f4c4
Vikas Shenoy <vikashenoy26@gmail.com>
2024-02-01 11:15:55 +1300

    Initial commit

---
+++ src/functions/Utils.test.ts
@@ -0,0 +1,7 @@
+import { expect, test, describe } from "vitest"
+
+describe("Utils", () => {
+  test("sample test", () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ src/functions/Utils.ts
@@ -0,0 +1,16 @@
+// 1. Write a function which sums two numbers
+
+// 2. Write a function which sums two numbers, but returns null if the answer is negative
+
+// 3. Write a function which takes an array of pokemon and returns the name of the pokemon with the highest level
+// Interface for Pokemon provided below:
+
+interface Pokemon {
+  name: string
+  level: number
+  evolution?: string
+}
+
+// 4. Write a function which takes an array of pokemon and returns an array of pokemon which have an evolution
+
+// 5. Given an array of integers and a target number, return indices of the two numbers such that they add up to target
