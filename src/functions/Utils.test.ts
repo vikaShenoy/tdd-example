e441765f2e74545e28f504adb8e73e6bfd6d59ea
radrah <raduwan.rahman@hotmail.com>
2024-02-05 12:28:49 +1300

    Completed 4/5 tests and functions

--- src/functions/Utils.test.ts
+++ src/functions/Utils.test.ts
@@ -1,7 +1,87 @@
 import { expect, test, describe } from "vitest"
 
+import { sumNum, sumNumPositivesOnly, Pokemon, highestLevelPokemon, evolvedPokemon, findIndicesForTargetNumber } from "./Utils"
+
 describe("Utils", () => {
-  test("sample test", () => {
-    expect(true).toBe(true)
+  describe('Q1', () =>{
+    test("should return sum of a and b", () => {
+    const expected = 5
+    const actual = sumNum(2,3)
+    expect(expected).toEqual(actual)
+    })
+  })
+
+  describe('Q2', () => {
+    test("should return sum of and b", () => {
+      expect(sumNumPositivesOnly(2,3)).toEqual(5)
+    })
+ 
+    test('should return null when the sum of a and b is negative', () => {
+      expect(sumNumPositivesOnly(2, -5)).toBeNull()
+    })
+
+    test('should return 0 when the sum of a and b is "0"', () => {
+      expect(sumNumPositivesOnly(2,-2)).toEqual(0)
+    })
+  })
+
+  describe('Q3', () => {
+    test('should return null if the pokemon array is empty', () => {
+      const poke = highestLevelPokemon([])
+      expect(poke).toBeNull()
+    })
+
+    test('should return the highest level pokemon name', () => {
+      const pokemon: Pokemon[] = [{name: 'bob', level:1}, {name: 'steve', level: 2}, {name: 'broseph', level:4}]
+      const poke = highestLevelPokemon(pokemon)
+
+      expect(poke).toBe('broseph')
+    })
+
+    test('should return the last highest level pokemon name is there are multiple of the same level', () => {
+      const pokemon: Pokemon[] = [{name: 'bob', level:1}, {name: 'steve', level: 2}, {name: 'broseph', level:4}, {name: 'jeff', level: 4}]
+      const poke = highestLevelPokemon(pokemon)
+
+      expect(poke).toBe('jeff')
+    })
+  })
+  
+  describe('Q4', () => {
+    test('should return null if pokemon array is empty', () => {
+      const poke = evolvedPokemon([])
+      expect(poke).toBeNull()
+    })
+
+    test('should return null if no evolved pokemons in the array', () => {
+      const pokemon:Pokemon[] = [{name:'bob', level:2}, {name:'steve', level:3}, {name: 'jeff', level: 4}]
+      const poke = evolvedPokemon(pokemon)
+      expect(poke).toBeNull()
+    })
+
+    test('should return an array with evolved pokemons', () => {
+      const pokemon:Pokemon[] = [{name:'bob', level:2, evolution:'bobby'}, {name:'steve', level:3}, {name: 'jeff', level: 4, evolution:'jefferson'}, {name:'pp', level:6, evolution:'popo'}]
+      const poke = evolvedPokemon(pokemon)
+      const evolvedPoke:Pokemon[] = [{name:'bob', level:2, evolution:'bobby'}, {name: 'jeff', level: 4, evolution:'jefferson'}, {name:'pp', level:6, evolution:'popo'}]
+      expect(poke).toStrictEqual(evolvedPoke)
+    })
+  })
+
+  describe('Q5', () => {
+    test('should return null if an empty array is passed', () => {
+      expect(findIndicesForTargetNumber([], 9)).toBeNull()
+    })
+
+    test('should return null if no two numbers can be added to the target number', () => {
+      const numbers = [1,2]
+      const target = 20
+      expect(findIndicesForTargetNumber(numbers, target)).toBeNull()
+    })
+
+    test('should return the indices of the two numbers that add upto the target number', () => {
+      const numbers = [1,2,3,4,5,6,7]
+      const target = 9
+      expect(findIndicesForTargetNumber(numbers, target)).toStrictEqual('The indices are 1 and 6')
+    })
+    
   })
 })
--- src/functions/Utils.ts
+++ src/functions/Utils.ts
@@ -1,16 +1,66 @@
 // 1. Write a function which sums two numbers
+function sumNum(a: number, b: number): number {
+  return a+b
+}
 
 // 2. Write a function which sums two numbers, but returns null if the answer is negative
+
+function sumNumPositivesOnly(a:number, b:number):number | null {
+  return a+b >= 0 ? a+b : null 
+}
 
 // 3. Write a function which takes an array of pokemon and returns the name of the pokemon with the highest level
 // Interface for Pokemon provided below:
 
-interface Pokemon {
+export interface Pokemon {
   name: string
   level: number
   evolution?: string
 }
 
+function highestLevelPokemon(pokemon:Pokemon[]):string | null {
+  if(pokemon.length === 0) {
+    return null
+  }
+  let currentMaxLevel = 0
+  let pokeName = ''
+  pokemon.forEach((poke: Pokemon) => {
+    if(poke.level >= currentMaxLevel) {
+      currentMaxLevel = poke.level
+      pokeName = poke.name
+    }
+  })
+  return pokeName
+}
+
 // 4. Write a function which takes an array of pokemon and returns an array of pokemon which have an evolution
 
+function evolvedPokemon( pokemon: Pokemon[] ): Pokemon[] | null {
+  if(pokemon.length === 0) {return null}
+  let evolvedPoke:Pokemon[] = []
+  pokemon.forEach((poke:Pokemon) => {
+    if('evolution' in poke) {
+      evolvedPoke.push(poke)
+    }
+  })
+  if(evolvedPoke.length === 0){
+    return null
+  }
+  return evolvedPoke
+}
+
 // 5. Given an array of integers and a target number, return indices of the two numbers such that they add up to target
+
+function findIndicesForTargetNumber( numbers: Number[], targetNumber: number ):string | null {
+  if(numbers.length === 0){
+    return null
+  }
+  let index1;
+  let index2;
+  numbers.forEach((num:number) => {
+    
+  })
+  return '0'
+}
+
+export { sumNum, sumNumPositivesOnly, highestLevelPokemon, evolvedPokemon, findIndicesForTargetNumber}
