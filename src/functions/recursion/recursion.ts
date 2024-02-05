a2a6e941ab00ff8b9303a0c05944125d63637122
Vikas Shenoy <vikashenoy26@gmail.com>
2024-02-05 13:35:19 +1300

    Add stub files for recursion

---
+++ src/functions/recursion/recursion.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from "../../types/pokemon";
+
+// 1. Implement a function which takes a list of pokemon and starting pokemon
+// and returns the name of the starting pokemon's maximum evolution in the collection
+export function findMaxEvolution(pokemonId: number, pokemon: Pokemon[]): string | null {
+  return null
+}
