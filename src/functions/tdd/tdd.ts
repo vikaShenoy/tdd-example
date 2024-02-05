fa5ff5935785bf5b7ee7690ea8c45fc69e351ef5
Vikas Shenoy <vikashenoy26@gmail.com>
2024-02-05 13:28:25 +1300

    Move Pokemon interface into separate file

--- src/functions/tdd/tdd.ts
+++ src/functions/tdd/tdd.ts
@@ -3,13 +3,6 @@
 // 2. Write a function which sums two numbers, but returns null if the answer is negative
 
 // 3. Write a function which takes an array of pokemon and returns the name of the pokemon with the highest level
-// Interface for Pokemon provided below:
-
-interface Pokemon {
-  name: string
-  level: number
-  evolution?: string
-}
 
 // 4. Write a function which takes an array of pokemon and returns an array of pokemon which have an evolution
 
---
+++ src/types/pokemon.ts
@@ -0,0 +1,6 @@
+export interface Pokemon {
+  id: number
+  name: string
+  level: number
+  evolution?: number
+}
