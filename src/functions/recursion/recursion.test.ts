5b5c608dc5e7b55d6633222f0ee12a60478cc316
radrah <raduwan.rahman@hotmail.com>
2024-02-07 12:01:02 +1300

    recursion rad complete

--- src/functions/recursion/recursion.test.ts
+++ src/functions/recursion/recursion.test.ts
@@ -1,8 +1,29 @@
 import { expect, test, describe } from "vitest"
 import { findMaxEvolution } from "./recursion"
+import { Pokemon } from "../../types/pokemon"
 
-describe("Recursion functions", () => {
-  test("Can find a pokemon's final evolution", () => {
+describe("Recursion functions, find pokemon's final evolution", () => {
+  test("should return null when an empty list is passed", () => {
     expect(findMaxEvolution(0, [])).toBe(null)
+  })
+
+  test("should return the inital pokemon name if it doesnt evolve", () => {
+    const pokemons: Pokemon[] = [{id:1, name:'pika', level: 12}, {id:3, name:'pikac', level: 125}, {id:13, name:'pikachu', level: 2}]
+    expect(findMaxEvolution(1, pokemons)).toBe('pika')
+  })
+
+  test("should return the evolved pokemon name if it evolves", () => {
+    const pokemons: Pokemon[] = [{id:1, name:'pika', level: 12, evolution: 13}, {id:3, name:'pikac', level: 125}, {id:13, name:'pikachu', level: 2}]
+    expect(findMaxEvolution(1, pokemons)).toBe('pikachu')
+  })
+
+  test("should return the last evolved pokemon name if it evolves", () => {
+    const pokemons: Pokemon[] = [{id:1, name:'pika', level: 12, evolution: 3}, {id:3, name:'pikac', level: 125, evolution:14}, {id:13, name:'pikachu', level: 2}, {id:14, name:'pikachuchu', level: 2}]
+    expect(findMaxEvolution(1, pokemons)).toBe('pikachuchu')
+  })
+
+  test("should return null if the id provided is not in the list", () => {
+    const pokemons: Pokemon[] = [{id:1, name:'pika', level: 12, evolution: 3}, {id:3, name:'pikac', level: 125, evolution:14}, {id:13, name:'pikachu', level: 2}, {id:14, name:'pikachuchu', level: 2}]
+    expect(findMaxEvolution(134, pokemons)).toBe(null)
   })
 })
--- src/functions/recursion/recursion.ts
+++ src/functions/recursion/recursion.ts
@@ -3,5 +3,15 @@ import { Pokemon } from "../../types/pokemon";
 // 1. Implement a function which takes a list of pokemon and starting pokemon
 // and returns the name of the starting pokemon's maximum evolution in the collection
 export function findMaxEvolution(pokemonId: number, pokemon: Pokemon[]): string | null {
-  return null
+  if(pokemon.length === 0 ){
+    return null
+  }
+  const startingPokemon = pokemon.find((pokemon) => pokemon.id === pokemonId)
+  if(startingPokemon === undefined){
+    return null
+  }
+  if(startingPokemon.evolution === undefined){
+    return startingPokemon!.name
+  }
+  return (findMaxEvolution(startingPokemon.evolution!, pokemon))
 }
